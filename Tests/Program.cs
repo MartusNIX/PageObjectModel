3966545932eee909a6e37d687c9fe782fb19931d
Daria Martus <martusdarja@gmail.com>
2022-10-05 13:40:28 +0300

    Changed variables to var type

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -8,24 +8,29 @@ namespace Tests
         [Test]
         public void PageTests()
         {
-            InitPage init = new InitPage(driver);
-            init.ClickSignIn();
-            SigninPage login = new SigninPage(driver);
+            var initPage = new InitPage(driver);
+            initPage.ClickSignIn();
+
+            var loginPage = new SigninPage(driver);
             Thread.Sleep(500);
-            login.InsertEmailInInput("user@gmail.com");
-            login.InsertPasswordlInInput("123456");
-            login.ClickSignInButton();
+
+            loginPage.InsertEmailInInput("user@gmail.com");
+            loginPage.InsertPasswordlInInput("123456");
+            loginPage.ClickSignInButton();
             Thread.Sleep(1000);
-            string actualParagraphCreateAccount = login.CheckParagraphCreateAccountText();
-            string expectedParagraphCreateAccount = "Please enter your email address to create an account.";
+
+            var actualParagraphCreateAccount = loginPage.CheckParagraphCreateAccountText();
+            var expectedParagraphCreateAccount = "Please enter your email address to create an account.";
             Assert.AreEqual(expectedParagraphCreateAccount, actualParagraphCreateAccount);
             Console.WriteLine("expected = \"{0}\"\n  actual = \"{1}\"", expectedParagraphCreateAccount, actualParagraphCreateAccount);
-            string actualHeaderRegistredAccount = login.CheckHeaderRegistrationText();
-            string expectedHeaderRegistredAccount = "ALREADY REGISTERED?";
+
+            var actualHeaderRegistredAccount = loginPage.CheckHeaderRegistrationText();
+            var expectedHeaderRegistredAccount = "ALREADY REGISTERED?";
             Assert.AreEqual(expectedHeaderRegistredAccount, actualHeaderRegistredAccount);
             Console.WriteLine("expected = \"{0}\"\n  actual = \"{1}\"", expectedHeaderRegistredAccount, actualHeaderRegistredAccount);
-            string actualButtonCreateAccount = login.CheckButtonCreateAccountText();
-            string expectedButtonCreateAccount = "Create an account";
+
+            var actualButtonCreateAccount = loginPage.CheckButtonCreateAccountText();
+            var expectedButtonCreateAccount = "Create an account";
             Assert.AreEqual(expectedButtonCreateAccount, actualButtonCreateAccount);
             Console.WriteLine("expected = \"{0}\"\n  actual = \"{1}\"", expectedButtonCreateAccount, actualButtonCreateAccount);
         }
